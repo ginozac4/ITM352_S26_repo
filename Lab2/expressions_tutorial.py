4263a5f1cc57c61ca4d795fdaf6364ea36e03010
ginozac4 <ginozac4@hawaii.edu>
2026-01-22 17:29:02 -1000

    Create expressions_tutorial.py

---
+++ Lab2/expressions_tutorial.py
@@ -0,0 +1,64 @@
+#PART 2
+
+# age in days
+age = 19
+print(age * 365)
+
+# monthly expenses if i spend x per week
+weekly_spending = 60
+print(weekly_spending * 52 / 12)
+
+# how many hours you've been alive
+print(age * 365 * 24)
+
+# GPA on a 100 point scale if on a 4.0 scale
+gpa_4_scale = 3.81
+print(gpa_4_scale * 25)
+
+# create at least five more expressions using birth year, grad year, or 
+# other personal numbers.
+
+#years since graduation
+grad_year = 2024
+years_since_graduation = (2026 - grad_year)
+print(years_since_graduation)
+
+#sleep hours per week
+sleep_hours_nightly = 5
+print(sleep_hours_nightly * 7)
+
+#buldak ramen per pack per month cost
+buldak_ramen_cost = 6.5
+print(buldak_ramen_cost * 3)
+
+# average screen time per month
+screen_time_daily = 8
+print(screen_time_daily * 30)
+
+# turn 21 in years
+current_age = 19
+print(21 - current_age)
+
+
+#PART 3
+name = "Cassiddy Ginoza"
+age = 19
+hometown = "Honolulu, HI"
+major = "Management Information Systems"
+
+print(f"My name is {name} and I am {age} years old.")
+print("I am from", hometown, "and my major is", major + ".")
+print(f"It has been {years_since_graduation} years since I graduated in {grad_year}.")
+
+# Create at least 3 complex expressions using your personal data
+months_until_21 = (21 * 12) - (current_age * 12)
+current_age_in_months = current_age * 12
+months_minus_age_months = months_until_21 - current_age_in_months
+print(f"I have {months_until_21} months until I turn 21.")
+
+total_sleep_hours_year = sleep_hours_nightly * 365
+print(f"I sleep approximately {total_sleep_hours_year} hours in a year.")
+
+total_ramen_cost_year = buldak_ramen_cost * 3 * 12
+print(f"I spend about ${total_ramen_cost_year} on buldak ramen in a year.")
+

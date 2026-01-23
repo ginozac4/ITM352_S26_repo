87216da9c53b496a0898ac2a7e29d95c9aefd493
ginozac4 <ginozac4@hawaii.edu>
2026-01-22 17:29:12 -1000

    Create ex5.py

---
+++ Lab2/ex5.py
@@ -0,0 +1,8 @@
+#Ask user to input a sentence of their choosing. 
+#Calculate length of string and print it out.
+
+user_string = input("Please enter a sentence: ")
+string_length = len(user_string)
+
+print(f"You entered: {user_string}")
+print(f"The length of your sentence is: {string_length} characters.")

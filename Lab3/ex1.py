05a4ee6109f0d5ddbf0d7e3ca2a8f7e0c500bfcf
ginozac4 <ginozac4@hawaii.edu>
2026-01-27 13:00:53 -1000

    Update ex1.py

--- Lab3/ex1.py
+++ Lab3/ex1.py
@@ -1,3 +1,12 @@
-import cryptography
+from cryptography.fernet import Fernet
 
-print(cryptography.__version__)
+key = Fernet.generate_key()
+cipher_suite = Fernet(key) 
+#cipher suite is an object
+
+encoded_text = cipher_suite.encrypt(b"This is a really secret message.")
+print(f"Encoded text: {encoded_text}")
+#b is a bite string - used when you want raw data
+
+decoded_text = cipher_suite.decrypt(encoded_text)
+print(f"Decoded text: {decoded_text.decode('utf-8')}")

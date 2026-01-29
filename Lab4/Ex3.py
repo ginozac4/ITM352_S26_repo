1c8bbc0b904b6662b0d8e0aa2fdb603b0cbdee52
ginozac4 <ginozac4@hawaii.edu>
2026-01-29 13:25:07 -1000

    Create Ex3.py

---
+++ Lab4/Ex3.py
@@ -0,0 +1,10 @@
+responseValues = [5, 7, 3, 8]
+responseValues.append(0)
+print("After appending 0:", responseValues)
+
+#responseValues.insert(2,6)
+responseValues = responseValues[:2] + [6] + responseValues[2:] 
+    # take the list up to index 2, add 6, then add the rest of the list
+print("After inserting 6 at index 2:", responseValues)
+
+

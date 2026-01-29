c34f67e793f29e1e3345b6d9288897dcd1e9e3df
ginozac4 <ginozac4@hawaii.edu>
2026-01-29 13:25:05 -1000

    Create Ex2.py

---
+++ Lab4/Ex2.py
@@ -0,0 +1,12 @@
+#Define a list of survey response variables and store them in variable, then define 
+# tuple of respondent ID values. Use the .append() method to append the tuple to the 
+# list, then print out the list.
+
+'''responses = [5, 7, 3, 8]
+respondent_ids = (1012, 1035, 1021, 1053)
+responses.append(respondent_ids)
+print("Survey responses with respondent IDs:", responses)'''
+
+response_values = [(1012, 5), (1035, 7), (1021, 3), (1053, 8)]
+response_values.sort()
+print("Sorted survey responses with respondent IDs:", response_values)

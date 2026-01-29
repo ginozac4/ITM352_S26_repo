dacf4d7b17edc12fec25d9f5b26f839da764a4c4
ginozac4 <ginozac4@hawaii.edu>
2026-01-29 13:25:03 -1000

    Create Ex1.py

---
+++ Lab4/Ex1.py
@@ -0,0 +1,23 @@
+# string manipulation examples
+
+first = input("Enter your first name: ")
+middleIn = input("Enter your middle initial: ")
+last = input("Enter your last name: ")
+
+full_name = first + " " + middleIn + ". " + last
+print("Your full name is:", full_name)
+
+print(f"Your full name is: {first} {middleIn}. {last}")
+
+#use the percent operator method
+print("Your full name is: %s %s. %s" % (first, middleIn, last))
+#use the format method
+
+print("Your full name is: {} {}. {}".format(first, middleIn, last))
+
+#use the join method
+print("Your full name is: " + " ".join([first, middleIn + ".", last]))
+
+# use the format() method for a string and unpack the list as the argument
+name_parts = [first, middleIn + ".", last]
+print("Your full name is: {}".format(" ".join(name_parts)))

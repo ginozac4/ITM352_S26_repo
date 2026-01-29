5de45fc20550f381b8066c4849d454cccb398fd5
ginozac4 <ginozac4@hawaii.edu>
2026-01-29 13:25:09 -1000

    Create Ex4.py

---
+++ Lab4/Ex4.py
@@ -0,0 +1,5 @@
+survey_responses = (1012, 1035, 1021, 1053)
+print("Original survey responses tuple:", survey_responses)
+# survey_responses.append(1011) # This will raise an AttributeError
+survey_responses = survey_responses + (1011,)
+print("After adding 1011: ", survey_responses)

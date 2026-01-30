9f0e9fbd57c55fc129ad534b4ab91e6688f6c71f
ginozac4 <ginozac4@hawaii.edu>
2026-01-30 07:06:35 -1000

    Update Ex4.py

--- Lab4/Ex4.py
+++ Lab4/Ex4.py
@@ -1,3 +1,6 @@
+# Name: Cassiddy Ginoza
+# Date: Jan. 29, 2026
+
 survey_responses = (1012, 1035, 1021, 1053)
 print("Original survey responses tuple:", survey_responses)
 # survey_responses.append(1011) # This will raise an AttributeError

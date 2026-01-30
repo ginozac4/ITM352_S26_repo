11eb90affeeffecb2cf079765ee3d6eee1be1432
ginozac4 <ginozac4@hawaii.edu>
2026-01-29 21:30:31 -1000

    Update UseModule.py

--- Lab3/UseModule.py
+++ Lab3/UseModule.py
@@ -1,4 +1,4 @@
-import HandyMath as HM
+'''import HandyMath as HM
 
 number1 = float(input("Enter the first number: "))
 number2 = float(input("Enter the second number: "))
@@ -17,4 +17,20 @@ min_value = HM.min(number1, number2)
 print(f"The minimum between {number1} and {number2} is {min_value}") 
 
 sqrt1 = HM.sqrt(number1)
-print(f"The square root of {number1} is {sqrt1}")
+print(f"The square root of {number1} is {sqrt1}")'''
+
+'''from HandyMath import max, min
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: ")) 
+max_value = max(number1, number2)
+print(f"The maximum between {number1} and {number2} is {max_value}")
+min_value = min(number1, number2)
+print(f"The minimum between {number1} and {number2} is {min_value}")'''
+
+from HandyMath import apply_function, max, min, midpoint, exponent
+
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+print(apply_function(number1, number2, max))
+print(apply_function(number1, number2, min))
+print(apply_function(number1, number2, exponent))  

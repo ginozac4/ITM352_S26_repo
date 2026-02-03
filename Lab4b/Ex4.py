ecd84740b029a8f077de8c72e170a04bd5fe04d6
ginozac4 <ginozac4@hawaii.edu>
2026-02-03 13:14:31 -1000

    Create Ex4.py

---
+++ Lab4b/Ex4.py
@@ -0,0 +1,21 @@
+# Parse through the portions of an email address
+
+#Method 1: Using split() to separate username and domain
+email = input("Enter your email address: ")
+
+parts = email.split("@")
+#splits string into two on the @ sign
+username = parts[0]
+domain = parts[1]
+
+print("Username: ", username)
+print("Domain Full: ", domain)
+
+#Method 2: Using index() and slicing
+at_symbol_index = email.index("@")
+username_manual = email[:at_symbol_index]
+domain_manual = email[at_symbol_index + 1 :]
+# + 1 because we don't want to include the @ sign
+
+print("Username (manual): ", username_manual)
+print("Domain Full (manual): ", domain_manual)

ccb478b08ca2dc7b1550741e44a64741b69f41a0
ginozac4 <ginozac4@hawaii.edu>
2026-02-03 13:14:33 -1000

    Create Ex5.py

---
+++ Lab4b/Ex5.py
@@ -0,0 +1,15 @@
+sentence = input("Enter a sentence: ")
+
+# 1. Turn string into a list of words
+words = sentence.split(" ")
+print("List of words: ", words)
+
+#2. Reverse the list
+#no reverse() method for strings, so we put into list first
+words.reverse()
+print("Reversed list of words: ", words)
+
+#3. Join the reversed list back into a string
+new_sentence = " ".join(words)
+    # uses " " to join the strings
+print("Reversed sentences:", new_sentence)

9127eb2ac4d9e275f4f900cb5231fcd4178ac5aa
ginozac4 <ginozac4@hawaii.edu>
2026-02-08 10:02:33 -1000

    Create Dictionary.py

---
+++ Lab5/Dictionary.py
@@ -0,0 +1,21 @@
+country_capitals = {
+    "Germany": "Berlin",
+    "Canada": "Ottawa",
+    "England": "London"}
+
+print(country_capitals)
+
+print(country_capitals["Canada"])
+print(country_capitals["England"])
+
+country_capitals["Italy"] = "Rome"
+
+print(country_capitals)
+# with big data sets, might not be as efficient
+
+country_capitals["Italy"] = "Milan"
+print(country_capitals)
+
+print("Germany" in country_capitals)
+print("Spain" not in country_capitals)
+print("Korea" in country_capitals)

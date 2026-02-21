ddfa8374aaff1415e155644c3c1b375e8e5eb455
ginozac4 <ginozac4@hawaii.edu>
2026-02-21 12:18:20 -1000

    Update Ex4c.py

--- Lab7/Ex4c.py
+++ Lab7/Ex4c.py
@@ -1,19 +1,25 @@
 #create a function that will print if the purchase is within the budget or not
+# Name: Cassiddy Ginoza
+# Date: Feb. 19, 2026
 
 def check_budget(recent_purchases, budget):
-    total_spent = 0
     results = [] 
 
     for purchase in recent_purchases:
-        total_spent += purchase
-        if total_spent > budget:
+        if purchase > budget:
             results.append(f"This purchase is over budget: {purchase}")
         else:
             results.append(f"This purchase is within budget: {purchase}")
     
     return results
 
-recent_purchase = [36.13, 23.87, 103.35, 22.93, 11.62]
-budget = 50
+#recent_purchase = [500.70, 450.23, 200.58, 450.38, 92.34]
+#budget = 500
 
-check_budget(recent_purchase, budget)
+#recent_purchase = [3.40, 2.68, 67.23, 24.53, 23.94]
+#budget = 24
+
+recent_purchase = [100.00, 150.00, 200.00, 250.00, 300.00]
+budget = 200
+
+print(check_budget(recent_purchase, budget))

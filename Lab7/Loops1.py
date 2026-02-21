c990ab332400d5bae668de65e4c695e44cab5e0e
ginozac4 <ginozac4@hawaii.edu>
2026-02-21 10:26:45 -1000

    Create Loops1.py

---
+++ Lab7/Loops1.py
@@ -0,0 +1,4 @@
+guests = ["Alice", "Bob", "Charlie", "David", "Eve"]
+
+for guest in guests: #iterate through each item in the list and pull off one at time
+    print(f"Hello, {guest}! I would like to invite you to the party on Feb. 22nd!")

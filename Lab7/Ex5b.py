2843d4bf9cdcd3598f79521cdd2eb733675160f6
ginozac4 <ginozac4@hawaii.edu>
2026-02-21 12:17:59 -1000

    Create Ex5b.py

---
+++ Lab7/Ex5b.py
@@ -0,0 +1,12 @@
+# Name: Cassiddy Ginoza
+# Date: Feb. 19, 2026
+
+celebs = ("Taylor Swift", "Lionel Messi", "The Weeknd", "Keanu Reeves", "Angelina Jolie")
+ages = (36, 28, 26, 61, 50)
+
+celebs_list = list(celebs)
+ages_list = list(ages)
+
+celebs_dict = {"Celebrities": celebs_list, "Ages": ages_list}
+
+print(celebs_dict)

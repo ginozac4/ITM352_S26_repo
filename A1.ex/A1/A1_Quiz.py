8a95320708f717f6abdccf39dae8d554a2194b50
ginozac4 <ginozac4@hawaii.edu>
2026-03-04 08:08:29 -1000

    Update A1_Quiz.py

--- A1.ex/A1/A1_Quiz.py
+++ A1.ex/A1/A1_Quiz.py
@@ -2,23 +2,25 @@ from string import ascii_lowercase
 import A1_SouthParkQuestions
 import A1_FootballMascotQuestions
 
-def get_quiz_selection():
+def user_quiz_selection():
     print("Which quiz would you like to play?")
     print("1. South Park Questions")
     print("2. Football Mascot Questions")
-    
+    # prints the quiz name options for the user to choose from
     
     while True:
-        choice = input("Enter your choice (1, 2, or 3): ")
+        choice = input("Enter your choice (1 or 2): ")
         if choice == "1":
             return A1_SouthParkQuestions.QUESTIONS
         elif choice == "2":
             return A1_FootballMascotQuestions.QUESTIONS
         else:
             print("Invalid choice - please enter 1 or 2.")
+    # loop that prompts the user to enter a valid choice, and will loop a re-prompt until they do
 
 def quiz():
-    QUESTIONS = get_quiz_selection()
+    QUESTIONS = user_quiz_selection()
+    # gets the quiz questions from the user's selection and returns the questions (from the loop in the function)
     num_correct = 0
     for question_number, (question, (choices, explanation)) in enumerate(QUESTIONS.items(), start=1):
         # starts at 1, and unpacks the question, options, and explanation from the QUESTIONS dictionary
@@ -26,7 +28,7 @@ def quiz():
         print(question)
         
         correct_answer = choices[0]
-        #the correct answer is always the first option in the list
+        # the correct answer is always the first option in the list
 
         answer_choices = dict(zip(ascii_lowercase, sorted(choices)))
         # creates a dictionary, assigning letters a, b, c, and d to the options
@@ -58,11 +60,15 @@ def quiz():
     # Requirement 1:
     with open("quiz_history.txt", "a") as history_file:
         # opens the quiz_history.txt file so it can be appended to
-        history_file.write(f"Score: {num_correct} out of {len(QUESTIONS)}\n")
-        # tells the code what to write to the file and adds a new line after each score
+        #add the quiz name to the file
+        if QUESTIONS == A1_SouthParkQuestions.QUESTIONS:
+            history_file.write(f"South Park Quiz: {num_correct} out of {len(QUESTIONS)}\n")
+        elif QUESTIONS == A1_FootballMascotQuestions.QUESTIONS:
+            history_file.write(f'Football Mascot Quiz: {num_correct} out of {len(QUESTIONS)}\n')
+            # \n prints new line after each quiz result is added to the file
     
     play_again = input("Would you like to play again? (Y/N) ")
-    if play_again == "Y":
+    if play_again == "Y" or play_again == "y":
         quiz()
     else:
         print("Thanks for playing!")

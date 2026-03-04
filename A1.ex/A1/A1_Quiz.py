ceb2029b47fd5e95b85dc4da7bde0134053c6682
ginozac4 <ginozac4@hawaii.edu>
2026-03-04 07:49:55 -1000

    Update A1_Quiz.py

--- A1.ex/A1/A1_Quiz.py
+++ A1.ex/A1/A1_Quiz.py
@@ -1,8 +1,24 @@
 from string import ascii_lowercase
-import A1_QuizQuestions
-QUESTIONS = A1_QuizQuestions.QUESTIONS
+import A1_SouthParkQuestions
+import A1_FootballMascotQuestions
+
+def get_quiz_selection():
+    print("Which quiz would you like to play?")
+    print("1. South Park Questions")
+    print("2. Football Mascot Questions")
+    
+    
+    while True:
+        choice = input("Enter your choice (1, 2, or 3): ")
+        if choice == "1":
+            return A1_SouthParkQuestions.QUESTIONS
+        elif choice == "2":
+            return A1_FootballMascotQuestions.QUESTIONS
+        else:
+            print("Invalid choice - please enter 1 or 2.")
 
 def quiz():
+    QUESTIONS = get_quiz_selection()
     num_correct = 0
     for question_number, (question, (choices, explanation)) in enumerate(QUESTIONS.items(), start=1):
         # starts at 1, and unpacks the question, options, and explanation from the QUESTIONS dictionary
@@ -32,13 +48,14 @@ def quiz():
         if answer == correct_answer:
             print("Correct!")
             print(explanation)
-            # prints the explanation when user gets the answer correct
+            # Requirement 7: prints the explanation when user gets the answer correct
             num_correct += 1
         else:
             print(f"The answer is {correct_answer}, not {answer}.")
 
     print(f"You got {num_correct} out of {len(QUESTIONS)} correct.")
     
+    # Requirement 1:
     with open("quiz_history.txt", "a") as history_file:
         # opens the quiz_history.txt file so it can be appended to
         history_file.write(f"Score: {num_correct} out of {len(QUESTIONS)}\n")

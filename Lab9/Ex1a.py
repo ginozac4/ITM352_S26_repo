1c6543850ca38e0760bf0a4bdf7f93c36ef65b65
ginozac4 <ginozac4@hawaii.edu>
2026-03-12 21:48:08 -1000

    Update Ex1a.py

--- Lab9/Ex1a.py
+++ Lab9/Ex1a.py
@@ -1,5 +1,5 @@
 # Open a text file and print its type
-file_object = open("names.txt")
-
-print(type(file_object))
-file_object.close()
+#file_object = open("names.txt")'''
+with open("names.txt") as file_object:
+    print(type(file_object))
+    #file_object.close()

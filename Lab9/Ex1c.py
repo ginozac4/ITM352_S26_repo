6544988c5eecef851ffff63048c1051028e748c1
ginozac4 <ginozac4@hawaii.edu>
2026-03-12 21:48:10 -1000

    Create Ex1c.py

---
+++ Lab9/Ex1c.py
@@ -0,0 +1,6 @@
+file_object = open("names.txt")
+contents = file_object.read()
+contents_list = contents.split("\n")
+print(contents)
+print(f"Number of names: {len(contents_list)}") 
+file_object.close()

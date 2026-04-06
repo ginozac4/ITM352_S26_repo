4019f506710b08dfe143ed577c6f1a475e4ce60b
ginozac4 <ginozac4@hawaii.edu>
2026-04-06 00:12:55 -1000

    Create A1_FootballMascotQuestions.py

---
+++ A1/A1_FootballMascotQuestions.py
@@ -0,0 +1,8 @@
+QUESTIONS = { 
+   "What is the mascot for the Denver Broncos? ": (["A horse", "A wolf", "A lion", "A bear"], "The Denver Broncos' mascot is a horse."),
+   "What is the mascot for the Philadelphia football team? ": (["An eagle", "A zebra", "A cat", "A bear"], "The Philadelphia football team's mascot is an eagle."),
+   "What is the mascot for the Miami football team? ": (["A dolphin", "An eel", "A narwhal", "A cat"], "The Miami football team's mascot is a dolphin."),
+   "What is the mascot for the Kansas City Chiefs football team? ": (["A wolf", "An alpaca", "A cat", "A llama"], "The Kansas City Chiefs football team's mascot is a wolf."),
+   "What is the mascot for the San Francisco 49ers football team? ": (["A miner", "A shark", "A cat", "An ox"], "The San Francisco 49ers football team's mascot is a miner."),
+   "What is one of the mascots for the New Orleans Saints football team? ": (["A dog", "A bird", "A cat", "A bear"], "One of the New Orleans Saints football team's mascot is a dog."),
+}

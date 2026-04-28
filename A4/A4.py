de0dc7cb95bbbe1a2d0ad57fb80592760684fd1d
ginozac4 <ginozac4@hawaii.edu>
2026-04-27 20:23:52 -1000

    Create A4.py

---
+++ A4/A4.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, request, redirect, url_for
+import random
+import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return render_template('welcome.html')
